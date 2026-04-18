f20814644662d14b79dd93766b4d22d3a090b8e2
priwete <exetior000@gmail.com>
2026-04-18 16:10:26 +0300

    Update 103.py

--- 103.py
+++ 103.py
@@ -1,7 +1,7 @@
 from PIL import Image
 from PIL import ImageFont, ImageDraw
 dict = {"День рождения": 'otkritka.jpg', "Новый год": 'otkritka2.jpg', "8 марта": 'otkritka3.jpg', "23 февраля": 'otkritka4.jpg'}
-font = ImageFont.truetype("arial.ttf", 50)
+font = ImageFont.truetype("arialbd.ttf", 40)
 a = input('Ваш праздник: ')
 img = Image.open(dict[a])
 imgdrow = ImageDraw.Draw(img)
